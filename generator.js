d429452789597967f9edd8a74c8d44f166bb5aeb
tony0x59 <hanningkong@gmail.com>
2019-05-19 16:19:54 +0800

    add browserlist conf

--- generator.js
+++ generator.js
@@ -1,2 +1,16 @@
 module.exports = (api, options, rootOptions) => {
+    const pkg = {
+        // package.json
+        // publicPath: ''
+        browserslist: [
+            "last 2 versions",
+            "> 1%",
+            "not IE_Mob <= 11",
+            "not OperaMini all",
+            "not dead"
+        ],
+        devDependencies: {}
+    }
+
+    api.extendPackage(pkg);
 }
